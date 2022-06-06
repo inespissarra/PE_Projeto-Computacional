a74210a72a253050133171e6366b0ef7413f2e7a
inespissarra <ines.pissarra@tecnico.ulisboa.pt>
2022-06-07 00:55:27 +0100

    tudo comentado

--- Pergunta10/Pergunta10.R
+++ Pergunta10/Pergunta10.R
@@ -6,6 +6,9 @@ x = c()
 ma=c()
 maC=c() 
 quantis = qnorm(1-0.05/2)
+epsilon = 0.25
+lambda = 1.32
+lambdaC = 0.01
 
 for(i in 1:25){
   amp=c()
@@ -13,20 +16,20 @@ for(i in 1:25){
   n = i*100
   x = append(x, n)
   for (j in 1:1500){
-    amostra = rexp(n*0.75, 1.32)
-    amostraC = rexp(n*0.25, 0.01) 
+    amostra = rexp(n*(1-epsilon), lambda)
+    amostraC = rexp(n*epsilon, lambdaC) 
     amostraC = append(amostraC, amostra) # amostra contaminada
     
-    amostra=append(amostra, rexp(n*0.25, 1.32)) # amostra não contaminada
+    amostra=append(amostra, rexp(n*epsilon, lambda)) # amostra não contaminada
     
     lambdaC_mle = 1/mean(amostraC)
-    a = lambdaC_mle * (1-(qnorm(1-(0.05/2), mean=0, sd=1))/sqrt(n))
-    b = lambdaC_mle * (1+(qnorm(1-(0.05/2), mean=0, sd=1))/sqrt(n))
+    a = lambdaC_mle * (1-quantis/sqrt(n))
+    b = lambdaC_mle * (1+quantis/sqrt(n))
     ampC<-append(ampC, b-a)
     
     lambda_mle = 1/mean(amostra)
-    a = lambda_mle * (1-(qnorm(1-(0.05/2), mean=0, sd=1))/sqrt(n))
-    b = lambda_mle * (1+(qnorm(1-(0.05/2), mean=0, sd=1))/sqrt(n))
+    a = lambda_mle * (1-quantis/sqrt(n))
+    b = lambda_mle * (1+quantis/sqrt(n))
     amp<-append(amp, b-a)    
   }
   ma<-append(ma, mean(amp))
@@ -35,9 +38,9 @@ for(i in 1:25){
 
 data<-data.frame(x, ma, maC)
 
-ggplot(data, aes())+
-  geom_point(aes(x, ma, color="Não contaminada"), size=1.5)+
-  geom_point(aes(x, maC, color="Contaminada"), size=1.5)+
+ggplot(data, aes(x))+
+  geom_point(aes(y = ma, color="Não contaminada"), size=1.5)+
+  geom_point(aes(y = maC, color="Contaminada"), size=1.5)+
   scale_color_manual(name="Amostra", values=c("Não contaminada"="blue", "Contaminada"="red"))+
   labs(title = "Variação da amplitude de intervalos de confiança", x = "Dimensão da amostra", y = "Amplitude dos intervalos de confiança")
 

eaffddb980c5a803d04a79fda22bebd0e42e7aca
Inês Pissarra <95494630+inespissarra@users.noreply.github.com>
2022-06-16 21:23:03 +0100

    Update Pergunta1.R

--- Pergunta1/Pergunta1.R
+++ Pergunta1/Pergunta1.R
@@ -1,8 +1,10 @@
 library(readxl)
 library(ggplot2)
 ResiduosPerCapita <- read_excel("ResiduosPerCapita.xlsx", range = "A12:C43")
-dados_2004 = c(ResiduosPerCapita$`2004`[21],ResiduosPerCapita$`2004`[9],ResiduosPerCapita$`2004`[31]); dados_2018 = c(ResiduosPerCapita$`┴ 2018`[21],ResiduosPerCapita$`┴ 2018`[9],ResiduosPerCapita$`┴ 2018`[31])
-Países = c(ResiduosPerCapita$Anos[21], ResiduosPerCapita$Anos[9], ResiduosPerCapita$Anos[31]); Paises_data = data.frame(Países, dados_2004, dados_2018)
+dados_2004 = c(ResiduosPerCapita$`2004`[21],ResiduosPerCapita$`2004`[9],ResiduosPerCapita$`2004`[31]); 
+dados_2018 = c(ResiduosPerCapita$`┴ 2018`[21],ResiduosPerCapita$`┴ 2018`[9],ResiduosPerCapita$`┴ 2018`[31])
+Países = c(ResiduosPerCapita$Anos[21], ResiduosPerCapita$Anos[9], ResiduosPerCapita$Anos[31]);
+Paises_data = data.frame(Países, dados_2004, dados_2018)
 
 print(ggplot(Paises_data, aes(Países, fill = Anos)) +
   geom_bar(aes(y = dados_2004, fill = "2004"), stat = "identity", width = .3, position = position_nudge(x = -.15)) + 
